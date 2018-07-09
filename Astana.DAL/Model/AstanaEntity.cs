62ad20b66e86faa532d754bab36d8a661e317bce
KakharovZhanibek <37920502+KakharovZhanibek@users.noreply.github.com>
2018-07-10 00:32:26 +0600

    Add files via upload

---
+++ Astana.DAL/Model/AstanaEntity.cs
@@ -0,0 +1,43 @@
+﻿using Astana.BLL.ModelClient;
+using GeneratorName;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astana.DAL.Model
+{
+    public class AstanaEntity
+    {
+        public List<Client> GetClients()
+        {
+            Random rndGender = new Random();
+            Generator genName = new Generator();
+            Client client1 = new Client();
+            client1.login = "admin";
+            client1.password = "admin";
+            client1.fullName = genName.GenerateDefault((Gender)rndGender.Next(2))
+                .Replace("<center><b><font size=7>", "")
+                .Replace("</font></b></center>", "")
+                .Replace("\n","")
+                .Substring(1);
+
+            Client client2 = new Client()
+            { login = "user", password = "user",fullName= genName.GenerateDefault((Gender)rndGender.Next(2)).Replace("<center><b><font size=7>", "")
+                .Replace("</font></b></center>", "")
+                .Replace("\n", "")
+            .Substring(1)};
+
+            Client client3 = new Client("god", "god", "God");
+           
+
+            List<Client> clients = new List<Client>();
+            clients.Add(client1);
+            clients.Add(client2);
+            clients.Add(client3);
+            return clients;
+        }
+    }
+    
+}
---
+++ Astana.DAL/ModelClient/Client.cs
@@ -0,0 +1,31 @@
+﻿using Astana.BLL.ModelAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astana.BLL.ModelClient
+{
+    public class Client
+    {
+        public Client():this("","")
+        {           
+        }
+        public Client(string login,string pass):this(login, pass, "")
+        {
+        }
+        public Client(string login, string pass,string fullName)
+        {
+            accounts = new List<Account>();
+            this.login = login;
+            password = pass;
+            this.fullName = fullName;
+        }
+        public string login { get; set; }
+        public string fullName { get; set; }
+        public string password { get; set; }
+        public List<Account> accounts;
+        
+    }
+}
